d26106e83438e4ec6bf76dbdf0e81ba19c6976ce
jacovb <vbiljon@gmail.com>
2021-10-13 06:57:00 +0100

    added custom Sign In

--- components/SignIn.js
+++ components/SignIn.js
@@ -1,14 +1,44 @@
 import { Auth } from "aws-amplify";
+import SocialSignIn from "./SocialSignIn";
+import Input from "./Input";
 
-export default function SignIn() {
+export default function SignIn({ onChange, setUiState, signIn }) {
   return (
     <div>
-      <button onClick={() => Auth.federatedSignIn({ provider: "Google" })}>
-        Sign in with Google
+      <p className="text-3xl font-black">Sign in to your Account</p>
+      <div className="mt-10">
+        <label className="text-sm">Email</label>
+        <Input onChange={onChange} name="email" />
+      </div>
+      <div className="mt-4">
+        <label className="text-sm">
+          Password
+          <span
+            onClick={() => setUiState("forgotPassword")}
+            className="ml-8 sm:ml-48 text-pink-600"
+          >
+            Forgot your Password?
+          </span>
+        </label>
+        <Input onChange={onChange} name="password" type="password" />
+      </div>
+      <button
+        onClick={signIn}
+        className="text-white w-full mt-6 bg-pink-600 p-3 rounded"
+      >
+        Sign In
       </button>
-      <button onClick={() => Auth.federatedSignIn({ provider: "Facebook" })}>
-        Sign in with Facebook
-      </button>
+      <SocialSignIn />
+      <p className="mt-12 text-sm font-light">
+        Don't have an account?
+        <span
+          onClick={() => setUiState("signUp")}
+          role="button"
+          className="cursor-pointer text-pink-600"
+        >
+          Sign Up
+        </span>
+      </p>
     </div>
   );
 }
